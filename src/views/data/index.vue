98337472a868ddbd05882a1b4b68c31aaa76e498
poxiao58 <1782840861@qq.com>
2019-04-02 15:03:40 +0800

    数据总览--基本完成

--- src/views/data/index.vue
+++ src/views/data/index.vue
@@ -7,11 +7,14 @@
           <span :class="{'active': item.tab === showActive}" v-for="item in showbtnList" :key="item.index" @click="clickShowBtn(item.tab)">{{item.name}}</span>
         </div>
       </div>
-      <div class="n-dataList">
+      <div class="n-dataList" v-show="showTab">
         <div class="data-btn" :class="{'current-data':flagArr[index]}" v-for="(item,index) in videoList"  @click="showAreaEsnClk(index)" :key="item.index">{{item.name}}</div>
       </div>
+      <div class="n-dataList" v-show="!showTab">
+        <div class="data-btn" :class="{'current-data':flagTypeArr[index]}" v-for="(item,index) in videoTypeList"  @click="showEsnTypeClk(index)" :key="item.index">{{item.name}}</div>
+      </div>
       <div class="n-dataCount" ref="conBody" :style="'height:' + bodyH + 'rem'">
-        <el-row>
+        <el-row v-show="showTab">
           <el-col :span="8" class="colLi" v-for="(areaItem,index) in videoList" v-show="flagArr[index]" :key="areaItem.index">
             <div class="dataArea">
               <h3>{{areaItem.name}}</h3>
@@ -21,10 +24,19 @@
                 <span v-show="areaItem.id==esnItem.userArea.id">{{esnItem.name}}</span>
                 <em v-show="areaItem.id==esnItem.userArea.id"><i>{{esnItem.nowData}}</i>{{esnItem.unit}}</em>
               </el-col>
-              <!--<el-col :span="12">-->
-                <!--<span>室外温度</span>-->
-                <!--<em><i>17.2</i> ℃ </em>-->
-              <!--</el-col>-->
+            </el-row>
+          </el-col>
+        </el-row>
+        <el-row v-show="!showTab">
+          <el-col :span="8" class="colLi" v-for="(typeItem,index) in videoTypeList" v-show="flagTypeArr[index]" :key="typeItem.index">
+            <div class="dataArea">
+              <h3>{{typeItem.name}}</h3>
+            </div>
+            <el-row class="datalist">
+              <el-col :span="12" v-for="(esnItem,index) in esnList" :key="esnItem.index" >
+                <span v-show="typeItem.id==esnItem.cgData">{{esnItem.userArea.name}}>{{esnItem.name}}</span>
+                <em v-show="typeItem.id==esnItem.cgData"><i>{{esnItem.nowData}}</i>{{esnItem.unit}}</em>
+              </el-col>
             </el-row>
           </el-col>
         </el-row>
@@ -38,7 +50,9 @@ export default {
   data() {
     return {
       showActive: '1',
+      showTab:true,
       flagArr:[],
+      flagTypeArr:[],
       esnList:[],
       showbtnList: [
         {
@@ -51,6 +65,28 @@ export default {
         }
       ],
       videoList: [],
+      videoTypeList: [
+        {
+          id:'光照度',
+          name:'光照度'
+        },
+        {
+          id:'湿度',
+          name:'湿度'
+        },
+        {
+          id:'土壤湿度',
+          name:'土壤湿度'
+        },
+        {
+          id:'温度',
+          name:'温度'
+        },
+        {
+          id:'土壤温度',
+          name:'土壤温度'
+        },
+      ],
       bodyH: 12
     };
   },
@@ -101,10 +137,21 @@ export default {
     },
     clickShowBtn(val) {
       this.showActive = val
+      this.showTab=!this.showTab
+      for (let i=0;i<this.videoTypeList.length;i++){
+        this.$set(this.flagTypeArr,i,true)
+      }
+      for (let i=0;i<this.videoList.length;i++){
+        this.$set(this.flagArr,i,true)
+      }
     },
     showAreaEsnClk(index){
       let value=this.flagArr[index]
       this.$set(this.flagArr,index,!value)
+    },
+    showEsnTypeClk(index){
+      let value=this.flagTypeArr[index]
+      this.$set(this.flagTypeArr,index,!value)
     }
   }
 };
