3093b562bc6b9332dbc0b76f9153477c8d60384d
lyn7568 <1271778060@qq.com>
2019-07-02 16:00:41 +0800

    echart添加区域缩放

--- src/components/LineChart/index.vue
+++ src/components/LineChart/index.vue
@@ -117,6 +117,31 @@ export default {
             }
           }
         ],
+        dataZoom: [
+          {
+              type: 'slider',
+              show: true,
+              start: 94,
+              end: 100,
+              handleSize: 8
+          },
+          {
+              type: 'inside',
+              start: 94,
+              end: 100
+          },
+          {
+              type: 'slider',
+              show: true,
+              yAxisIndex: 0,
+              filterMode: 'empty',
+              width: 12,
+              height: '70%',
+              handleSize: 8,
+              showDataShadow: false,
+              left: '93%'
+          }
+        ],
         toolbox: {
           show: true,
           feature: {
